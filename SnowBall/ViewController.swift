525d00d7950b04ccccccb1c62e2055961696d633
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-03-08 09:19:59 +0900

    set calc function

--- SnowBall/ViewController.swift
+++ SnowBall/ViewController.swift
@@ -7,13 +7,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var reserveAmount: UITextField!
+    @IBOutlet weak var reserveYear: UITextField!
+    @IBOutlet weak var reserveMonth: UITextField!
+    @IBOutlet weak var yeild: UITextField!
+    
+    var amount = Int()
+    var year = Int()
+    var month = Int()
+    var investmentYeild = Double()
+    
+    @IBOutlet weak var totalSum: UILabel!
+    @IBOutlet weak var principal: UILabel!
+    @IBOutlet weak var profit: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.reserveAmount.keyboardType = UIKeyboardType.numberPad
+        self.reserveYear.keyboardType = UIKeyboardType.numberPad
+        self.reserveMonth.keyboardType = UIKeyboardType.numberPad
+        self.yeild.keyboardType = UIKeyboardType.numberPad
+        
+        reserveAmount.delegate = self
+        reserveYear.delegate = self
+        reserveMonth.delegate = self
+        yeild.delegate = self
+        
     }
 
-
+    @IBAction func calc(_ sender: Any) {
+        amount = Int(reserveAmount.text!)!
+        year = Int(reserveYear.text!)!
+        month = Int(reserveMonth.text!)!
+        investmentYeild = Double(yeild.text!)!
+        
+        let totalMonth = year * 12 + month
+        let monthlyInterest = investmentYeild / 12 / 100
+        let exponentiation = pow(1 + monthlyInterest, Double(totalMonth))
+        let total = Int(ceil((exponentiation - 1) / monthlyInterest * Double(amount)))
+        let principalTotal = amount * totalMonth
+        let profitTotal = total - principalTotal
+       
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = ","
+        formatter.groupingSize = 3
+        
+        totalSum.text = formatter.string(from: NSNumber(value: total))
+        principal.text = formatter.string(from: NSNumber(value: principalTotal))
+        profit.text = formatter.string(from: NSNumber(value: profitTotal))
+        
+        view.endEditing(true)
+        
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        reserveAmount.text = ""
+        reserveYear.text = ""
+        reserveMonth.text = ""
+        yeild.text = ""
+        
+        totalSum.text = "0"
+        principal.text = "0"
+        profit.text = "0"
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
