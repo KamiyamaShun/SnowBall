20dc1835f767fe65774ff78c0c73726145747d7c
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-04-01 17:55:49 +0900

    tab bar icon

---
+++ SnowBall/Controller/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  SnowBall
+//
+//  Created by 神山駿 on 2021/04/01.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}
